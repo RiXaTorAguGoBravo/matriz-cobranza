aaf8bf02bcaece3257b3cb59a7d15d3dd53ccf1d
Ricardo Xavier Torres Aguilar <ricardo.torres@gobravo.mx>
2025-03-12 12:55:55 -0600

    Actualización de consultas.sql

--- consultas.sql
+++ consultas.sql
@@ -1,26 +1,24 @@
--- CREDITOS
 SELECT
     credits.id AS id_credito,
     credits.deposit_reference AS referencia_deposito,
     credits.amount AS saldo_inicial,
     credits.annual_interest_rate AS tasa_interes_anual,
     credits.payment_amount AS cuota_mensual,
+    credits.term AS plazo,
     credits.operational_opening_date AS fecha_apertura,
-    credits.first_payment_date AS fecha_primer_pago
+    credits.first_payment_date AS fecha_primer_pago,
+    CASE 
+        WHEN NOT credits.open THEN LEAST(
+            (SELECT MAX(payments_2.date) FROM payments AS payments_2 WHERE payments_2.credit_id = credits.id),
+            credits.closing_date
+        )
+        ELSE NULL
+    END AS fecha_cierre, -- CONSIDERAMOS QUE LA FECHA DE CIERRE CORRESPONDE A LA FECHA DEL ULTIMO PAGO REALIZADO
+    payments.date AS fecha_pago,
+    payments.amount AS monto_pago
 FROM credits
+    LEFT JOIN payments ON credits.id = payments.credit_id
 WHERE
-    credits.product_id IN (7, 8, 9, 10, 11)
-    AND credits.country = 'mx'
-ORDER BY credits.id;
-
--- PAGOS
-SELECT
-    payments.credit_id AS id_credito,
-    payments.amount AS monto_pago,
-    payments.date AS fecha_pago
-FROM payments
-    JOIN credits ON payments.credit_id = credits.id
-WHERE
-    credits.product_id IN (7, 8, 9, 10, 11)
-    AND credits.country = 'mx'
-ORDER BY payments.credit_id, payments.date;
+    credits.country = 'mx'
+    AND credits.product_id IN (7, 8, 9, 10, 11)
+ORDER BY credits.id, payments.date;
